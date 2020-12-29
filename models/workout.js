c67a09ac72671c86695a097ccea7438149b817cc
Dan Chiev <danchiev2@Gmail.com>
2020-12-29 17:53:43 -0500

    added workout model

--- models/workout.js
+++ models/workout.js
@@ -3,16 +3,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction",
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount",
-  },
-  date: {
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "What type of workout are you performing?",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "What is the name of the workout?",
+      },
+      duration: {
+        type: Number,
+        required: "How long was the workout?",
+      },
+      weight: {
+        type: Number,
+        required: "How much weight was used (in pounds)?",
+      },
+      reps: {
+        type: Number,
+        required: "How many reps did you perform?",
+      },
+      sets: {
+        type: Number,
+        required: "How many sets did you perform?",
+      },
+    },
+  ],
+
+  day: {
     type: Date,
     default: Date.now,
   },

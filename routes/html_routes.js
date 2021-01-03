2fad92a8661ab9a61cb8ec9bf7e2219d40221f59
Dan Chiev <danchiev2@Gmail.com>
2021-01-03 10:51:42 -0500

    fixed html routes

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -3,12 +3,15 @@ const path = require("path");
 module.exports = function (app) {
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
+    console.log("Hello");
   });
 
   app.get("/exercise", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    console.log("Hello");
   });
   app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
+    console.log("Hello");
   });
 };
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const express = require("express");
 const mongojs = require("mongojs");
 const path = require("path");
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -19,8 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false,
 });
 
-app.use(require("./routes/api_routes.js"));
-app.use(require("./routes/html_routes.js"));
+require("./routes/api_routes.js")(app);
+require("./routes/html_routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);

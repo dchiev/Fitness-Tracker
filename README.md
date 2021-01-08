fefdcc30c74e0576a5c4960af1f3838e1b5d4b54
Dan Chiev <danchiev2@Gmail.com>
2021-01-08 18:45:43 -0500

    added toJSON and virtuals to model

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Fitness-Tracker
 
-change
+change again
--- models/workout.js
+++ models/workout.js
@@ -2,43 +2,53 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "What type of workout are you performing?",
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "What is the name of the workout?",
-      },
-      duration: {
-        type: Number,
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      distance: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-});
 
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "What type of workout are you performing?",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "What is the name of the workout?",
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return 5;
+});
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
